2b97d09aeb42f99d9aede3ab3a3f8753d52621ca
407-41-wannawat <56760@yupparaj.ac.th>
2026-06-26 11:31:04 +0700

    Update digtal_profile.py

--- digtal_profile.py
+++ digtal_profile.py
@@ -2,7 +2,7 @@ import steamlit as st
 st.title("✨ My Digital Profile")
 
 st.header("😁 แนะนำตัว")
-st.write("นายวรรณวัฒน์ โภชกรณ์").
+st.write("นายวรรณวัฒน์ โภชกรณ์")
 st.write("ชื่อเล่น ฮอร์โมน")
 st.write("ม.4/7 เลขที่ 41")
 
